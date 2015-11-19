09d0d0610acf4226a87178cbf0bd3869eb2e7114
wdhorton <wdt.horton@gmail.com>
2015-11-19 12:22:02 -0500

    Style StoryItem.

--- app/assets/javascripts/components/story_item.js.jsx
+++ app/assets/javascripts/components/story_item.js.jsx
@@ -1,16 +1,17 @@
 window.StoryItem = React.createClass({
   render: function () {
     return (
-      <li className="story">
+      <li className="story accepted">
         <header className="preview">
           <a className="expander"></a>
+          <a className="selector"></a>
           <span className="meta"></span>
           <span className="name">
             <span className="story-name">
               Shopper should be able to click on a product, and see all product details, including photos
             </span>
             <span className="labels">
-              <a className="label">shopping</a>
+              <a className="label std">shopping</a>
             </span>
           </span>
         </header>
