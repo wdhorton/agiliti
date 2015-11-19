451e765b3ea86b01eff1a982df42288da70beac8
wdhorton <wdt.horton@gmail.com>
2015-11-19 10:28:40 -0500

    Add HTML and CSS for Sidebar Footer.

--- app/assets/javascripts/components/sidebar_footer.js.jsx
+++ app/assets/javascripts/components/sidebar_footer.js.jsx
@@ -2,7 +2,13 @@ window.SidebarFooter = React.createClass({
   render: function () {
     return (
       <footer className="sidebar-footer">
-        
+        <a className="width-mode" title="Panel Width: Auto">auto</a>
+        <a className="fit-all" title="Fit Panels to Browser">fit all</a>
+        <form>
+          <a className="narrow"></a>
+          <a className="wide"></a>
+          <div className="slider"></div>
+        </form>
       </footer>
     );
   }
