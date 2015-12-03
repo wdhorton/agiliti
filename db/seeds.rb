8ba539de5f84abe004ce1de7fa1ae493098e9fbf
wdhorton <wdt.horton@gmail.com>
2015-12-02 20:58:17 -0500

    Debug seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 demo = Person.create!(name: "Demo User", email: "demo_user@example.com", username: "demouser", password: "password", company: "App Academy")
 personal = Account.create!(name: "Demo Personal", plan: "free", status: "active")
-AccountMembership.create!(person_id: william.id, account_id: personal.id, owner:true)
+AccountMembership.create!(person_id: demo.id, account_id: personal.id, owner:true)
 business = Account.create!(name: "Demo Business", plan: "free", status: "active")
 AccountMembership.create!(person_id: demo.id, account_id: business.id, owner:true)
 project = Project.create!(name: "Test project", start_time: "2015-11-18 09:21:37 -0500", account_id: personal.id)
