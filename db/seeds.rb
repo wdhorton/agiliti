deb0b00d9681fd702af207c2723130a9d31218b1
wdhorton <wdt.horton@gmail.com>
2015-12-02 20:53:05 -0500

    Update seeds to include demo user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-william = Person.create!(name: "William H", email: "william@example.com", username: "wdh", password: "password", company: "App Academy")
-personal = Account.create!(name: "William Personal", plan: "free", status: "active")
+demo = Person.create!(name: "Demo User", email: "demo_user@example.com", username: "demouser", password: "password", company: "App Academy")
+personal = Account.create!(name: "Demo Personal", plan: "free", status: "active")
 AccountMembership.create!(person_id: william.id, account_id: personal.id, owner:true)
+business = Account.create!(name: "Demo Business", plan: "free", status: "active")
+AccountMembership.create!(person_id: demo.id, account_id: business.id, owner:true)
 project = Project.create!(name: "Test project", start_time: "2015-11-18 09:21:37 -0500", account_id: personal.id)
 
 ["accepted", "started", "unstarted", "unscheduled"].each.with_index do |state, i|
