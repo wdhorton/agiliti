2d6386d3f1a3731fe2a7a4104bc35780dff9db1c
wdhorton <wdt.horton@gmail.com>
2015-12-03 22:51:21 -0500

    Add card for updating basic profile information.

--- app/assets/javascripts/components/profile/ProfilePage.js
+++ app/assets/javascripts/components/profile/ProfilePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from '../dashboard/Header';
 import Footer from '../dashboard/Footer';
 import UserManagementHeader from './UserManagementHeader';
+import { currentUser } from '../../stores/current_user_store';
 
 export default class ProfilePage extends Component {
   render() {
@@ -13,9 +14,27 @@ export default class ProfilePage extends Component {
           <UserManagementHeader />
           <div className="content group">
             <div className="profile-page settings">
-              <div className="column">
-              </div>
-              <div className="column">
+              <div className="card">
+                <div className="header">
+                  <h3>My Profile</h3>
+                  <div className="actions edit">
+                    <a href="#" className="edit-button header-button">Edit</a>
+                  </div>
+                </div>
+                <ul className="rows read">
+                  <li>
+                    <h4>User name</h4>
+                    <div>{ currentUser().username }</div>
+                  </li>
+                  <li>
+                    <h4>Name</h4>
+                    <div>{ currentUser().name }</div>
+                  </li>
+                  <li>
+                    <h4>Initials</h4>
+                    <div>{ currentUser().initials }</div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
