b6fd2371762b1d540bcddf1b8c11875e647678c9
wdhorton <wdt.horton@gmail.com>
2015-12-07 20:56:12 -0500

    Extend tests on Person.find_or_create_by_auth_hash.

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     name  { Faker::Name.name}
     email { Faker::Internet.email }
     password  { Faker::Internet.password }
+
+    trait :oauth do
+      provider  "google"
+      uid { Faker::Number.number(12) }
+    end
+
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -25,4 +25,45 @@ RSpec.describe Person, :type => :model do
 
   end
 
+  describe '.find_or_create_by_auth_hash' do
+
+    it "finds a person if provider and uid are in the database" do
+      person = create(:person, :oauth)
+      auth_hash = { provider: person.provider, uid: person.uid }
+      result = Person.find_or_create_by_auth_hash(auth_hash)
+      expect(result).to eq(person)
+    end
+
+    describe "when provider and uid are not in the database" do
+      before(:all) do
+        @count = Person.count
+        fname = Faker::Name.first_name
+        lname = Faker::Name.last_name
+        email = Faker::Internet.email
+
+        @auth_hash = {
+          info: {
+            first_name: fname,
+            last_name: lname,
+            email: email,
+            name: fname + " " + lname
+          },
+          provider: "google",
+          uid: Faker::Number.number(12)
+        }
+
+        @person = Person.find_or_create_by_auth_hash(@auth_hash)
+      end
+
+      it "adds a person to the database" do
+        expect(Person.count).to eq(@count + 1)
+      end
+
+      it "returns a new person" do
+        expect(@person).to be_a(Person)
+      end
+    end
+
+  end
+
 end
