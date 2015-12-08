6f753f51d206a4db328b7146734b2fecd9baa931
wdhorton <wdt.horton@gmail.com>
2015-12-07 19:54:51 -0500

    Write specs for Person model.

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Person, :type => :model do
+
+  describe ".find_by_credentials" do
+
+    it "returns the correct person given an email and password" do
+      
+    end
+
+    it "returns nil with incorrect password" do
+
+    end
+
+    it "returns nil if email is not in the database" do
+
+    end
+
+  end
+
+end
