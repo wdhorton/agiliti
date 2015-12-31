a4a8d4788dc2987264fc7cdac8e3ec7090abd932
wdhorton <wdt.horton@gmail.com>
2015-12-31 13:03:34 -0500

    Complete SignUpPage tests.

--- frontend/spec/components/SignUpPage.spec.js
+++ frontend/spec/components/SignUpPage.spec.js
@@ -2,6 +2,9 @@ import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import SignUpPage from '../../javascripts/components/sign_up/SignUpPage';
+import SignUpHeader from '../../javascripts/components/sign_up/SignUpHeader';
+import SignUpContent from '../../javascripts/components/sign_up/SignUpContent';
+import SignInFooter from '../../javascripts/components/sign_in/SignInFooter';
 
 function setup() {
   const renderer = TestUtils.createRenderer();
@@ -18,6 +21,14 @@ describe("components", () => {
 
       expect(output.type).toBe('div');
       expect(output.props.className).toBe('sign-in-page');
+    });
+
+    it("renders child components", () => {
+      const children = setup().props.children;
+
+      expect(children[0].type).toBe(SignUpHeader);
+      expect(children[1].type).toBe(SignUpContent);
+      expect(children[2].type).toBe(SignInFooter);
     });
   });
 });
